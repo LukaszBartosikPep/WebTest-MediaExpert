0d66461a78359fac7771b6aef95a12281ee51261
lbartosik <lbartosik@pepco.eu>
2023-12-04 12:20:51 +0100

    "Finished test suite"

--- src/main/java/com/example/demo/pages/TestingLocalShop.java
+++ src/main/java/com/example/demo/pages/TestingLocalShop.java
@@ -12,34 +12,33 @@ public class TestingLocalShop extends General{
 
     public static final By findShop=By.xpath("/html/body/div[1]/div[2]/header[2]/div[1]/div/div/div[3]/a[2]");
     public static final By giveShopLocation=By.xpath("//div[@id='accordion']//input[@id='address']");
-    public static final By searchButton=By.xpath("//div[@id='accordion']//button[@id='btn-search-shop']");
+//    public static final By searchButton=By.xpath("//div[@id='accordion']//button[@id='btn-search-shop']");
     public static final By shopDetailsLocator=By.xpath("//div[@id='shopList']/div/div[2]");
-    public static final By clickForClear =By.xpath("//h2[text()='Znajdź elektromarket w pobliżu:']");
+//    public static final By clickForClear =By.xpath("//h2[text()='Znajdź elektromarket w pobliżu:']");
     public static final By liveResult=By.xpath("//div[@class='live-results show']/div[1]");
     public static final By shopDataAddress=By.xpath("//div[@class='tree']//ul[1]/li[2]");
     public static final By shopDataPostalCode=By.xpath("//div[@class='tree']//ul[1]/li[3]");
    public static final By closePopup=By.xpath("//div[@class='modal-body']/button[@aria-label='Zamknij' and @data-dismiss='modal']");
-    public void clickSearchShop(){
+
+
+   public void clickSearchShop(){
         click(findShop);
     }
 
     public void setAddress(){
         setField(giveShopLocation, "Poznań");
-        WebElement shopField=getElement(giveShopLocation);
-//        shopField.sendKeys(Keys.ENTER);
+//        WebElement shopField=getElement(giveShopLocation);
+
         click(liveResult);
 
     }
 
-//    public void clickSearch(){
-//        click(searchButton);
-//    }
+
     public void clickSpecificShop(){
 
 
         explicitWait(shopDetailsLocator);
         click(shopDetailsLocator);
-
         scrollToData();
         click(closePopup);
     }
